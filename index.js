dd7ac00e483c11af67af73919b7b42f49276d692
Your Name <galla.venkatesh@i-exceed.com>
2024-09-03 14:35:13 +0530

    Displaying current date,day and time

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let i;
-let n = 10;
-for (i = 0; i <= n; i += 2) {
-  console.log(i);
-}
+const dayList = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const date = new Date();
+
+const day = date.getDay();
+console.log("Today is :", day);
+
+let hour = date.getHours();
+const minute = date.getMinutes();
+const second = date.getSeconds();
+
+const timeZone = hour >= 12 ? "PM" : "AM";
+hour = hour >= 12 ? hour - 12 : hour;
+console.log("Current time is :", hour, timeZone, ":", minute, ":", second);
