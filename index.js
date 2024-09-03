d29654f69c07be2c3f123f2548d4c1e38a460701
Your Name <galla.venkatesh@i-exceed.com>
2024-09-03 19:10:04 +0530

    Tip Calculator

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-const image = document.querySelector("img");
-const textUsed = document.querySelector(".text");
-const userName = document.querySelector(".username");
+const btnEl = document.getElementById("calculate");
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+const totalSpan = document.getElementById("total");
 
-const persons = [
-  {
-    name: "Chandu",
-    photoUrl: "./images/girl1.jfif",
-    text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
-  },
-  {
-    name: "Rosetta Q",
-    photoUrl: "./images/girl2.jfif",
-    text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
-  },
-  {
-    name: "Constantine V",
-    photoUrl: "./images/girl3.jfif",
-    text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
-  },
-];
+btnEl.addEventListener("click", calculateTotal);
 
-let index = 0;
+function calculateTotal() {
+  const billValue = Number(billInput.value);
+  const tipPercent = Number(tipInput.value);
 
-slideShow();
+  const tipValue = (tipPercent / 100) * billValue;
 
-function slideShow() {
-  const { name, photoUrl, text } = persons[index];
-  image.src = photoUrl;
-  textUsed.innerText = text;
-  userName.innerText = name;
-  index++;
-  if (index === persons.length) {
-    index = 0;
-  }
-  setTimeout(() => {
-    slideShow();
-  }, 1000);
+  const totalValue = billValue + tipValue;
+
+  totalSpan.innerText = totalValue.toFixed(2);
 }
