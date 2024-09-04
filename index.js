74a6053a181ada185bd136548c9c894600b1598a
Your Name <galla.venkatesh@i-exceed.com>
2024-09-04 16:20:53 +0530

    Recipe Book App

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-const display = document.querySelector(".age");
-const age = document.querySelector(".para");
-const text = document.getElementById("inpt");
+const API_KEY = "275d58779ccf4e22af03e792e8819fff";
 
-display.addEventListener("click", displayAge);
+const list = document.querySelector(".list");
 
-function displayAge() {
-  const calculate = text.value;
-  if (calculate) {
-    const date = new Date();
-    const year = Number(date.getFullYear());
-    const previousYear = Number(calculate.split("-")[0]);
-    const presentYear = year - previousYear;
-    age.innerText = `Year age is ${presentYear}  years old`;
-  } else {
-    alert("please enter your birthday");
-  }
+async function getRecipies() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+  );
+  const responseData = await response.json();
+  return responseData.recipes;
 }
+
+function display(recipies) {
+  recipies.forEach((recipie) => {
+    const li = document.createElement("li");
+    li.classList.add("list-item");
+
+    list.appendChild(li);
+
+    const img = document.createElement("img");
+    console.log("recipie", recipie);
+    img.src = recipie.image;
+    img.alt = "food image";
+    li.appendChild(img);
+
+    const heading = document.createElement("h3");
+    heading.innerText = recipie.title;
+    li.appendChild(heading);
+
+    const description = document.createElement("p");
+    description.innerHTML = `<strong>Ingredients:</strong> ${recipie.extendedIngredients.map(
+      (ingredient) => ingredient.original
+    )}`;
+    li.appendChild(description);
+
+    const link = document.createElement("a");
+    link.href = recipie.sourceUrl;
+    link.innerText = "VIEW RECIPE";
+    li.appendChild(link);
+  });
+}
+
+async function init() {
+  const recipies = await getRecipies();
+  display(recipies);
+}
+init();
